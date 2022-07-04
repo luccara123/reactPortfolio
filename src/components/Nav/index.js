ededb05212f147e8a22433ec1c3f99d589f3d017
Lucca Araujo <lucca.ara7@gmail.com>
2022-07-03 21:13:59 -0700

    added about component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function About(){
+    return(
+        <section id="about">
+
+            <div className="title-container">
+                <h2 className="section-title"> ABOUT ME </h2>
+            </div>
+
+            <div className="half-container">
+
+                <div classNmae="main-photo-container">
+                    <img alt="my-photo"/>
+                </div>
+
+                <article classNmae="text-container">
+                    <p> My name is Lucca Araujo</p>
+                </article>
+            </div>
+        </section>
+    )
+}
+
+export default About;
